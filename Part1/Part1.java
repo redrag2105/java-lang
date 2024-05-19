4c3a7d2b6eaa34e8685ce0885f0610f01b01fa21
redrag2105 <akiettran2005@gmail.com>
2024-05-19 15:40:07 +0700

    test

--- Part1/Part1.java
+++ Part1/Part1.java
@@ -1,43 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Part1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
+
 public class Part1 {
     public static void main(String[] args) {
-        int rows;
-        int cols;
-        int[][] matrix;
+        try (Scanner sc = new Scanner(System.in)) {
+            int rows = getDimension(sc, "Enter the number of rows: ");
+            int cols = getDimension(sc, "Enter the number of columns: ");
+            int[][] matrix = getMatrix(rows, cols);
 
-        rows = getRows();
-        cols = getCols();
-        matrix = getMatrix(rows, cols);
+            inputMatrix(sc, matrix);
+            displayMatrix(matrix);
 
-        inputMatrix(matrix, rows, cols);
-        displayMatrix(matrix, rows, cols);
-
-        int sum = getSum(matrix, rows, cols);
-        float average = getAverage(sum, rows, cols);
-        printResult(sum, average);
+            int sum = getSum(matrix);
+            float average = getAverage(sum, matrix.length, matrix[0].length);
+            printResult(sum, average);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter an integer.");
+        }
     }
 
-    public static int getRows(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows: ");
-        return sc.nextInt();
-    }
-
-    public static int getCols(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of columns: ");
+    public static int getDimension(Scanner sc, String prompt){
+        System.out.print(prompt);
         return sc.nextInt();
     }
 
@@ -45,33 +31,36 @@ public class Part1 {
         return new int[rows][cols];
     }
 
-    private static void inputMatrix(int[][] matrix, int rows, int cols) {
-        Scanner sc = new Scanner(System.in);
+    private static void inputMatrix(Scanner sc,int[][] matrix) {
         System.out.println("Enter the matrix: ");
-        for (int i =0; i < rows; i++){
-            for (int j =0; j < cols; j++){
+        for (int i =0; i < matrix.length; i++){
+            for (int j =0; j < matrix[i].length; j++){
                 System.out.print("m[" + i + "][" + j + "]= ");
                 matrix[i][j] = sc.nextInt();
             }
         }
     }
 
-    private static void displayMatrix(int[][] matrix, int rows, int cols) {
+    private static void displayMatrix(int[][] matrix) {
         System.out.println("Matrix inputted: ");
-        for (int i =0; i < rows; i++){
-            for (int j =0; j < cols; j++){
+        int i =0;
+        while (i < matrix.length) {
+            for (int j =0; j < matrix[i].length; j++){
                 System.out.format("%3d", matrix[i][j]);
             }
             System.out.println("\n");
+            i++;
         }
     }
 
-    private static int getSum(int[][] matrix, int rows, int cols) {
+    private static int getSum(int[][] matrix) {
         int sum = 0;
-        for (int i = 0; i < rows; i++){
-            for (int j =0; j < cols; j++){
+        int i = 0;
+        while (i < matrix.length) {
+            for (int j =0; j < matrix[i].length; j++){
                 sum += matrix[i][j];
             }
+            i++;
         }
         return sum;
     }
